95b497240e8028e97174e5ff6062843b2536b2c3
5kywa1k3r <daoduyanh.tin47@gmail.com>
2025-03-06 00:27:42 +0700

    update restart check git

--- docker/restart.sh
+++ docker/restart.sh
@@ -3,7 +3,9 @@
 APP_PATH="/root/tuyen/lms/docker"
 BRANCH="master"
 SITE_NAME="lms.demo"
-
+git fetch origin $BRANCH
+LOCAL_COMMIT=$(git rev-parse HEAD)  
+REMOTE_COMMIT=$(git rev-parse origin/$BRANCH)
 CONTAINER_NAME=$(docker ps --format "{{.Names}}" | grep frappe)
 
 if [ -z "$CONTAINER_NAME" ]; then
@@ -23,18 +25,28 @@ if git diff --quiet HEAD origin/$BRANCH; then
     exit 0
 else
     echo "Changes detected! Pulling latest code..."
-    git reset --hard HEAD
+    git reset --hard origin/$BRANCH
     git pull origin $BRANCH
 fi
 
 echo "Updating Frappe..."
 docker exec -it "$CONTAINER_NAME" bash -c "
-    cd /home/frappe/frappe-bench/lms
-    git reset --hard
-    git pull
+    APP_PATH='/home/frappe/frappe-bench/apps/lms'
+
+    if [ ! -d \"\$APP_PATH\" ]; then
+        echo 'Getting lms app...'
+        cd /home/frappe/frappe-bench/apps
+        bench get-app https://5kywa1k3r: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@github.com/Flamesh/lms.git
+    else
+        echo 'lms already exists, updating...'
+        git reset --hard origin/$BRANCH
+        git pull origin $BRANCH
+    fi
+
     bench update --patch
-    bench --site "$SITE_NAME" migrate
+    bench --site \"$SITE_NAME\" migrate
     bench restart
 "
+
 
 docker logs -f "$CONTAINER_NAME"
