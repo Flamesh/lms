67c01a23a9c1418ce67342a686347ea6174e817d
5kywa1k3r <daoduyanh.tin47@gmail.com>
2025-03-06 01:50:05 +0700

    setup cache, socket, role

--- docker/restart.sh
+++ docker/restart.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+FORCE=false
+
+if [[ "$1" == "-f" ]]; then
+    FORCE=true
+fi
 
 APP_PATH="/root/tuyen/lms/docker"
+APP_PATH_DOCKER="/home/frappe/frappe-bench/apps/lms"
 BRANCH="master"
 SITE_NAME="lms.demo"
 git fetch origin $BRANCH
@@ -22,30 +28,39 @@ git fetch origin $BRANCH
 
 if git diff --quiet HEAD origin/$BRANCH; then
     echo "No changes found. Skipping update & restart."
-    exit 0
+    if ["$FORCE" = false]; then 
+    	exit 0
+    fi
+    
+    echo "force updating..." 
 else
     echo "Changes detected! Pulling latest code..."
 fi
 
 echo "Updating Frappe..."
 docker exec -it "$CONTAINER_NAME" bash -c "
-    APP_PATH='/home/frappe/frappe-bench/apps/lms'
 
-    if [ ! -d \"\$APP_PATH\" ]; then
+    if [ ! -d "$APP_PATH_DOCKER" ]; then
         echo 'Getting lms app...'
-        cd /home/frappe/frappe-bench/apps
+        cd /home/frappe/frappe-bench/
         bench get-app https://5kywa1k3r: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@github.com/Flamesh/lms.git
     else
         echo 'lms already exists, updating...'
+        cd /home/frappe/frappe-bench/apps/lms
         git reset --hard origin/$BRANCH
         git pull origin $BRANCH
     fi
 
+    cd /home/frappe/frappe-bench/
     bench update --patch
-    bench --site \"$SITE_NAME\" migrate
+    bench build
+    bench migrate
+    bench setup socketio
+    bench setup supervisor
+    bench setup redis
     bench restart
 "
 
 git reset --hard origin/$BRANCH
 git pull origin $BRANCH
-docker logs -f "$CONTAINER_NAME"
+docker logs -100f "$CONTAINER_NAME"
