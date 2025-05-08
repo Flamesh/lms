74afa8351c6537ae124ce48cbbb08101748f8890
QTC-dang-tuyen <147590471+QTC-dang-tuyen@users.noreply.github.com>
2025-05-08 21:34:02 +0700

    1

--- frontend/src/pages/Programs.vue
+++ frontend/src/pages/Programs.vue
@@ -174,19 +174,14 @@ const createProgram = (close) => {
 	const user_id = Cookie.get('user_id')
 	const user_lang = Cookie.get('user_lang')
 	const system_user = Cookie.get('system_user')
-	// console.log(sid, full_name, user_id, user_lang, system_user)
-	// console.log(sid, decodeURIComponent(document.cookie))
 	
-	const headers = {
-		'Content-Type': 'application/json',
-		Cookie: `sid=${sid}; system_user=${system_user}; full_name=${full_name}; user_id=${user_id}; user_lang=${user_lang};`,
-	}
-	console.log(headers.Cookie)
 	fetch('http://localhost:8000/api/method/lms.lms.custom_api.program.add_program', {
 		method: 'POST',
+		credentials: 'include',
 		headers: {
 			'Content-Type': 'application/json',
-			Cookie: `sid=${sid}; system_user=${system_user}; full_name=${full_name}; user_id=${user_id}; user_lang=${user_lang};`,
+			"Cookie": `sid=${sid}; system_user=${system_user}; full_name=${full_name}; user_id=${user_id}; user_lang=${user_lang};`,
+			
 		},
 		body: JSON.stringify({
 			title: title.value,
